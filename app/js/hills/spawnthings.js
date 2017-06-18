dfa0ef86c2f5ce8e3c966e470583a442910de0d7
Max Koehler <max@koehler-kn.de>
2017-06-18 11:48:57 +0100

    add particle system

--- app/js/hills/spawnthings.js
+++ app/js/hills/spawnthings.js
@@ -27,6 +27,11 @@ AFRAME.registerComponent('spawnships', {
             animationEl.setAttribute('dur', getRandomInt(25000, 35000));
             animationEl.setAttribute('to', '800.667 ' + getRandomInt(20, 100) +  ' ' + getRandomInt(-50, 50));
             shipEl.appendChild(animationEl)
+                 
+   //         var particleEl = document.createElement('a-entity')
+  //          particleEl.setAttribute('particle-system', 'duration: 20; velocityValue: .1 .1 .1;velocitySpread: .0001 .0001 .0001;accelerationValue: .001 .001 .001;accelerationSpread: .001 .001 .001; texture: ../images/smokeparticle.png; color: #FFFFFF');
+ //           shipEl.appendChild(particleEl)
+
             setTimeout(spawn, getRandomInt(5000, 15000));
         };
         spawn();
